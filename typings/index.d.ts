5776a23ccb81636bc99678a6cae8e0f200946d67
Nishant George Agrwal <nishantagrwal12993@gmail.com>
2016-08-09 02:10:23 +0530

    added typings for ES6

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/express/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/cheerio/index.d.ts" />
